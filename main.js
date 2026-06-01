848c6feee5a6f70d7d6d75f80d5427f2e7ceb232
CarCVroom <bjornarlundm@gmail.com>
2026-06-01 13:04:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ const bgSpeed = 0.35;
 function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        ctx.drawImage(bgimg2, 0, bgY - 1, canvas.width, canvas.height);
+        ctx.drawImage(bgimg2, 0, bgY + 1, canvas.width, canvas.height);
         ctx.drawImage(bgimg, 0, bgY - canvas.height, canvas.width, canvas.height);
 
         bgY += bgSpeed;
